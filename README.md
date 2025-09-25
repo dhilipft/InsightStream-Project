c8d11a267b15e70a2d3c693bcabdd5c408f4f9a8
Dhilip Kumar <dhilipkumar0907@gmail.com>
2025-09-25 11:17:29 +0530

    InsightStream

---
+++ README.md
@@ -0,0 +1,40 @@
+# 𝗜𝗻𝘀𝗶𝗴𝗵𝘁𝗦𝘁𝗿𝗲𝗮𝗺 📰
+
+InsightStream is a modern, feature-rich news application built with React. It provides a dynamic and engaging platform for users to browse, search, and save news articles from around the world. The application features a sleek user interface with light and dark modes, a simulated multi-user environment for saving articles, and a clean, responsive design.
+
+# Getting Started with React App
+
+To get a local copy up and running, follow these simple steps.
+
+## Prerequisites
+Node.js and npm: Make sure you have Node.js and npm installed on your machine. You can download them from nodejs.org \
+Get a free API Key from https://newsapi.org
+
+# Installation
+
+## Clone the repository:
+
+Bash\
+`git clone https://github.com/dhilipft/Project-InsightStream.git`
+
+## Navigate into the project directory:
+
+Bash\
+`cd Project-InsightStream`
+
+## Install NPM packages:
+
+Bash\
+`npm install`
+
+Enter your API key in src/context/GeneralContext.jsx:
+
+JavaScript
+const apiKey = 'YOUR_NEWS_API_KEY'; 
+
+## Start the development server:
+
+Bash\
+`npm start`
+
+The application will be available at http://localhost:3000.
---
+++ src/context/GeneralContext.jsx
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+
+export const GeneralContext = React.createContext();
+
+const GeneralContextProvider = ({children}) => {
+
+    const [topNews, setTopNews] = useState([])
+
+    const [businessNews, setBusinessNews] = useState([]);
+    const [technologyNews, setTechnologyNews] = useState([]);
+    const [politicsNews, setPoliticsNews] = useState([]);
+
+    useEffect(() => { 
+        fetchTopNews() 
+        fetchBusinessNews()
+        fetchPoliticsNews()
+        fetchTechnologyNews()
+      }, [])
+    
+      const fetchTopNews = async () => {
+        try {
+          const response = await axios.get("https://newsapi.org/v2/everything?q=popular&apiKey=b55a68ba3d824788bd8618ff5bddbacf");
+          setTopNews(response.data.articles);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+      const fetchBusinessNews = async () => {
+        try {
+          const response = await axios.get("https://newsapi.org/v2/everything?q=business&apiKey=b55a68ba3d824788bd8618ff5bddbacf");
+          setBusinessNews(response.data.articles);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      const fetchPoliticsNews = async () => {
+        try {
+          const response = await axios.get("https://newsapi.org/v2/everything?q=politics&apiKey=b55a68ba3d824788bd8618ff5bddbacf");
+          setPoliticsNews(response.data.articles);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      const fetchTechnologyNews = async () => {
+        try {
+          const response = await axios.get("https://newsapi.org/v2/everything?q=technology&apiKey=b55a68ba3d824788bd8618ff5bddbacf");
+          setTechnologyNews(response.data.articles);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+    
+  return (
+    <GeneralContext.Provider value={{topNews, businessNews, technologyNews, politicsNews}} >{children}</GeneralContext.Provider>
+  )
+}
+
+export default GeneralContextProvider
